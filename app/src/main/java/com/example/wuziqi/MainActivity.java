d2af7cd20235caf59bfc56229b8474330c30d7cd
leijingxing <1276753397@qq.com>
2019-04-16 00:23:34 +0800

    修复按钮错误

--- app/src/main/java/com/example/wuziqi/MainActivity.java
+++ app/src/main/java/com/example/wuziqi/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.wuziqi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private Button button;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Intent intent = new Intent(MainActivity.this,MusicServer.class);
+		final Intent intent = new Intent(MainActivity.this,MusicServer.class);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		startService(intent);
 
+		button = (Button) findViewById(R.id.Onemoregame);
+		button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               onCreate(null);
+            }
+        });
 	}
 
 
@@ -25,11 +35,6 @@ public class MainActivity extends AppCompatActivity {
 		return true;
 	}
 
-    public void Onemoregame(View view){
-
-		onCreate(null);
-
-    }
 
 	public void Quit(View view) {
 		finish();
